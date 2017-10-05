b525aec614db6f59db761f4dd39efd2d1e5be520
Tomas Chmelevskij <T.Chmelevskij@gmail.com>
2017-10-05 13:40:10 +0100

    Add simple search

--- src/App.js
+++ src/App.js
@@ -7,18 +7,31 @@ class App extends Component {
   constructor(){
     super();
     this.state = {cakes:[]};
+
+    this.search = this.search.bind(this)
+  }
+
+  search(event){
+    let searchTerm = event.target.value.toLowerCase()
+    let searchedCakes = cakes.filter(({title}) => title.toLowerCase().includes(searchTerm))
+    this.setState({cakes:searchedCakes})
   }
 
   componentDidMount(){
     this.setState({cakes});
   }
   render() {
+    let {cakes}= this.state
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Cakes!!!</h1>
+          <input type="text" onChange={this.search}/>
         </header>
-        <ul> <CakeList {...this.state}/></ul>
+        { cakes.length
+          ? <CakeList cakes={this.state.cakes}/>
+          : <h2>No cakes matching search</h2>
+        }
       </div>
     );
   }
--- src/CakeList.js
+++ src/CakeList.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import './CakeList.css'
 
 const CakeList = ({cakes}) => (
-  cakes.map((cake,i) => (
-    <li key={ i }>
-      <h2> {cake.title} </h2>
-      <p> {cake.desc} </p>
-      <img src={cake.image} alt={cake.title}/>
-    </li>
-  ))
+  <ul className="CakeList">
+    {
+      cakes.map((cake,i) => (
+        <li key={ i }>
+          <h2> {cake.title} </h2>
+          <p> {cake.desc} </p>
+          <img src={cake.image} alt={cake.title}/>
+        </li>
+      ))
+    }
+  </ul>
 )
 
 export default CakeList
