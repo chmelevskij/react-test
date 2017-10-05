8ba2c3788de935bc0de184cfda01b3b9771f78ae
Tomas Chmelevskij <T.Chmelevskij@gmail.com>
2017-10-06 00:27:11 +0100

    Change text to textarea

--- src/CakeForm.js
+++ src/CakeForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CakeForm = ({handleInput, title, desc, image, onSubmit}) => (
   <form >
     <input value={title} onChange={handleInput} name="title" type="text" placeholder="title" required/>
-    <input value={desc}  onChange={handleInput} name="desc"  type="text" placeholder="description"/>
+    <textarea value={desc} name="desc" onChange={handleInput} placeholder="description" cols="30" rows="10"></textarea>
     <input value={image} onChange={handleInput} name="image" type="url" placeholder="image url"/>
     <input type="submit" onClick={onSubmit}/>
   </form>
