c405f3fb858cf3f37cb1fc75e666fba70542e632
Tomas Chmelevskij <T.Chmelevskij@gmail.com>
2017-10-06 01:29:28 +0100

    Basic styles for tablet

--- src/CakeForm.js
+++ src/CakeForm.js
@@ -4,12 +4,12 @@ import './CakeForm.css'
 const CakeForm = ({handleInput, title, desc, image, onSubmit}) => (
   <form className="CakeForm">
     <label htmlFor="title"> Title </label>
-    <input id="title" value={title} onChange={handleInput} name="title" type="text" placeholder="Title" required/>
+    <input className="CakeForm-input" id="title" value={title} onChange={handleInput} name="title" type="text" placeholder="Title" required/>
     <label htmlFor="desc"> Description </label>
-    <textarea id="desc" value={desc} name="desc" onChange={handleInput} placeholder="Description" cols="30" rows="10"></textarea>
+    <textarea  className="CakeForm-input" id="desc" value={desc} name="desc" onChange={handleInput} placeholder="Description" cols="30" rows="10"></textarea>
     <label htmlFor="image"> Image </label>
-    <input id="image" value={image} onChange={handleInput} name="image" type="url" placeholder="Image URL"/>
-    <button className="CakeForm-submit" type="submit" onClick={onSubmit}>Add</button>
+    <input className="CakeForm-input" id="image" value={image} onChange={handleInput} name="image" type="url" placeholder="Image URL"/>
+    <button className="CakeForm-submit CakeForm-input" type="submit" onClick={onSubmit}>Add</button>
   </form>
 )
 
